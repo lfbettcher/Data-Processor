e7cd9cd8f954429454f4e3d9441e82c8d1c15855
lfbettcher <57979763+lfbettcher@users.noreply.github.com>
2020-06-24 15:10:11 -0700

    Reads data into map

--- WindowsFormCore/ProcessSkyline.cs
+++ WindowsFormCore/ProcessSkyline.cs
@@ -24,15 +24,33 @@ namespace WindowsFormCore
                 int totalRows = worksheet.Dimension.Rows;
 
                 // Get set of compounds
-                HashSet<string> compounds = new HashSet<string>();
-                
+                Dictionary<string, List<KeyValuePair<string, string>>> dataMap = 
+                    new Dictionary<string, List<KeyValuePair<string, string>>>();
+
                 for (int i = 1; i <= totalRows; i++)
                 {
-                    compounds.Add(worksheet.Cells[i, 1].Value.ToString());
+                    string compound = worksheet.Cells[i, 1].Value.ToString();
+                    string sample = worksheet.Cells[i, 3].Value.ToString();
+                    string area = worksheet.Cells[i, 10].Value.ToString();
+
+                    KeyValuePair<string, string> pair = new KeyValuePair<string, string>(sample, area);
+
+                    if (dataMap.ContainsKey(compound))
+                    {
+                        List<KeyValuePair<string, string>> pairList = dataMap[compound];
+                        pairList.Add(pair);
+                        dataMap[compound] = pairList;
+                    }
+                    else
+                    {
+                        List<KeyValuePair<string, string>> pairList = new List<KeyValuePair<string, string>>();
+                        pairList.Add(pair);
+                        dataMap.Add(compound, pairList);
+                    }
                 }
 
-                int numCompounds = compounds.Count - 1;
-                //System.Diagnostics.Debug.WriteLine(numCompounds);
+                int numCompounds = dataMap.Count - 1;
+                System.Diagnostics.Debug.WriteLine(numCompounds);
 
                 /*
                 string peptideColContent = string.Empty;
